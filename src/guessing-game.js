01851027ac31af5faba7676fd7fb1927c4b23055
Anna Evstigneeva <evannetta@gmail.com>
2019-10-17 09:35:27 +0300

    add my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.current = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
+  guess() {
+    let range = this.max - this.min;
+    if (!(range & 1)) {
+      this.current = range / 2 + this.min;
+    } else {
+      this.current = Math.trunc(range / 2) + 1 + this.min;
     }
+    return this.current;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.current;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.current;
+  }
 }
 
 module.exports = GuessingGame;
